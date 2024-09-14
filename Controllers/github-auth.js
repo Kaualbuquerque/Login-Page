bbea605c068484d82821f681de0d1f0469eea571
Kauã Albuquerque <kaualbuquerque44@gmail.com>
2024-09-13 22:30:49 -0300

    Autenticação com github

--- Controllers/github-auth.js
+++ Controllers/github-auth.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 passport.use(new GitHubStrategy({
     clientID: "Ov23lin9vuF2bpLW6GSu",
     clientSecret: "b5a35743a6b4b4d4c66227d7c5b583c83ea73d1a",
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    callbackURL: "https://kaualbuquerque.github.io/Login-Page/auth/github/callback"
 },
     function (accessToken, refreshToken, profile, done) {
         return done(null, profile);
